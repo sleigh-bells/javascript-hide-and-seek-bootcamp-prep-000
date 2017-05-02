bba1a98d8958dce0acef7bdb30868dea80a62fc7
Brian Lee <blees.email@gmail.com>
2017-05-02 00:06:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('#app ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  const lis = document.querySelector('#grand-node').querySelectorAll('div');
+  var current = document.querySelector('#grand-node');
+  var next = {};
+  for (let i = 0; i < lis.length; i++) {
+    current = current.querySelector('div');
+  }
+  return current;
+}
